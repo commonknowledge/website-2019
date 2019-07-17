a317f8d78f083b109f3b8c3912826d19585a7bec
Jan Baykara <janbaykara@gmail.com>
2019-07-17 11:17:47 +0100

    fix: Build process

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,16 +5,9 @@
  */
 
 // You can delete this file if you're not using it
-exports.onCreateWebpackConfig = ({
-  stage,
-  rules,
 const webpackFixFs = require('./node/webpackFixFs').default
 
 exports.onCreateWebpackConfig = args => {
   // required fix for package `qrcode.js`
   webpackFixFs(args)
-    node: {
-      fs: 'empty',
-    },
-  })
 }
