aaa38d8a1393c5a6f83634599f66efc1a2bfa3c4
Chris Devereux <xcd@chris.dev>
2020-01-29 15:32:18 +0000

    adds metomic

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -25,5 +25,24 @@ export const onRenderBody = ({ setHeadComponents }) => {
       crossOrigin
       charSet="utf-8"
     />,
+    <script
+      type="text/x-metomic"
+      data-micropolicy="statistics"
+      dangerouslySetInnerHTML={{
+        __html: `
+var _paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://commonknowledgecoop.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/commonknowledgecoop.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+    `.trim(),
+      }}
+    />,
   ])
 }
