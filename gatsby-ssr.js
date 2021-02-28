c6605068240ef05baebc2da4b1bf1be42245241e
Alex Andrews <alex@alexandrews.info>
2021-02-28 20:54:38 +0000

    Add new cookie banner

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -22,36 +22,32 @@ export const wrapPageElement = ({ element }) => (
 
 export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   setHeadComponents([
-    <script
-      key={1}
-      src="https://config.metomic.io/config.js?id=prj:099e4e54-0922-481a-826c-52839ea79e1f"
-      crossOrigin
-      charSet="utf-8"
-    />,
-    <script
-      key={2}
-      src="https://consent-manager.metomic.io/embed.js"
-      crossOrigin
-      charSet="utf-8"
-    />,
-    <script
-      key={3}
-      type="text/x-metomic"
-      data-micropolicy="statistics"
+    <script key={1} type="text/javascript" 
       dangerouslySetInnerHTML={{
         __html: `
-var _paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://commonknowledgecoop.matomo.cloud/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/commonknowledgecoop.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-})();
-    `.trim(),
+        var _iub = _iub || [];
+_iub.csConfiguration = {"consentOnContinuedBrowsing":false,"whitelabel":false,"lang":"en","siteId":2174512,"cookiePolicyId":80929539,"cookiePolicyUrl":"https://commonknowledge.coop/privacy-policy/", "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#00e8a2","acceptButtonCaptionColor":"white","customizeButtonColor":"#212121","customizeButtonCaptionColor":"white","rejectButtonDisplay":true,"rejectButtonColor":"#00e8a2","rejectButtonCaptionColor":"white","position":"float-bottom-right","textColor":"white","backgroundColor":"#000001" }};
+          `.trim(),
+    }}/>,
+    <script key={2} type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>,
+    <script
+      key={3}
+      className="_iub_cs_activate"
+      data-iub-purposes="3"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var _paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+          var u="https://commonknowledgecoop.matomo.cloud/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/commonknowledgecoop.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+          `.trim(),
       }}
     />,
   ])
